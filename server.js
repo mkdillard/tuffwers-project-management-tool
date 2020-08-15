0f2b4d680a76d81d0e9422e5b1129735166bfcfb
Michael Dillard <mkdillard@hotmail.com>
2020-08-15 18:18:49 -0400

    Dealing with refresh issue

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
-const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback');
 const path = require('path');
 
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+app.use(bodyParser.json());
+app.use(cors());
+
+const historyOptions = {
+    index: '/'
+}
+
+app.use(history(historyOptions));
+
+// app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.get('/index.html', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.get('/*', express.static(path.join(__dirname, 'dist')));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Starting server on port ${port}`);
+});
